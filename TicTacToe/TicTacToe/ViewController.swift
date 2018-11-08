56858baddd825fac935db7907e98191cfed27b1e
vaslee <ltx0503@gmail.com>
2018-11-08 17:36:29 -0500

    start of project

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstButton: GameButton!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
   }
 
 
